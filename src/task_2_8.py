3ddde9542a86265894082d52500c0cbd0ff259c6
Radik Agliamov <aglyam92@mail.ru>
2025-10-29 09:43:48 +0300

    module 2 done

---
+++ src/task_2_8.py
@@ -0,0 +1,16 @@
+nums = [1, 2, 3, 45, 356, 569, 600, 705, 923]
+
+
+def search(number: id) -> bool:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        i = (left + right) // 2
+
+        if nums[i] == number:
+            return True
+        elif nums[i] > number:
+            left = i + 1
+        else:
+            right = i - 1
+    return False
